0f2998a9c48447bfafc59e447d9fdffe8ccfb0fe
Guilherme Lima <glimaramos013@gmail.com>
2023-10-19 09:37:18 -0300

    Create file: type-enum

---
+++ src/types/type-enum/index.ts
@@ -0,0 +1,18 @@
+enum Colors {
+  Red, // 0
+  Green, // 1
+  Blue = 30, // 30
+  Purple = 'Purple',
+}
+
+enum Colors {
+  Pink = 1,
+  Yellow,
+  Cyan = 'Cyan',
+}
+
+console.log(Colors[0]);
+console.log(Colors.Blue);
+console.log(Colors['Purple']);
+console.log('--------------------');
+console.log(Colors);
